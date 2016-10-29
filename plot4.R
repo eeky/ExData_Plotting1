73a19df947a3613b94bc7779b20afb5d60b9edd2
eeky <luishngama@gmail.com>
2016-10-29 20:55:19 -0200

    Exploratory Data Analysis - Week 1 Assignemnt

---
+++ plot4.R
@@ -0,0 +1,68 @@
+##########################################################
+#
+#Exploratory Data Analysis Course - Week 1 Assignment
+#
+#Created by Luis Gama
+#
+#09-28-2016
+#
+##########################################################
+
+#Assignment dataset download
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, "./data/ElectricPwrCons.zip")
+
+#Unziping the compressed file
+unzip("./data/ElectricPwrCons.zip", exdir = "./data")
+
+#Estimating how much memmory will be needed to load the file into R
+#2,075,269 lines x 9 cols x 8 bytes/numeric = 149,419,368 bytes or ~149,41MB.
+#My current system has 16GB of RAM, so that's fine
+
+#We need all samples generated in 1/2/2007 and 2/2/2007.
+#So 60 minutes * 48 hours = 2880 rows + 1 row for Saturday to appear in the x-axis :-)
+
+#I take a look in the file using Notepad++ and saw that the last reading
+#before 1/2/2007 was in 31/1/2007;23:59:00, so I used it as a starting
+#point for the grep function inside skip param.
+
+electricData <- read.table("./data/household_power_consumption.txt", 
+                           header = FALSE,
+                           sep = ";",
+                           col.names = c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"),
+                           na.strings = "?",
+                           colClasses = c("Date", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),
+                           skip = grep("31/1/2007;23:59:00", readLines("./data/household_power_consumption.txt")), nrows = 2880)
+
+#Setting the screen device to fit 4 plots
+par(mfrow = c(2, 2))
+
+#Using lubridate to convert from char to date
+require("lubridate")
+
+#Concatenating Date and Time
+datetime <- dmy_hms(paste(electricData$Date, electricData$Time, sep = " "))
+
+#Changing locale to English, so weekdays are shown as the assignment requires
+Sys.setlocale("LC_TIME", "English")
+
+#Plot 1
+with(electricData, plot(Global_active_power ~ datetime, xlab = "", ylab = "Global Active Power", type = "l"))
+
+#Plot 2
+with(electricData, plot(Voltage ~ datetime, xlab = "datetime", ylab = "Voltage", type = "l"))
+
+#Plot 3
+with(electricData, { 
+        plot(Sub_metering_1 ~ datetime, xlab = "", ylab = "Energy sub metering", type = "l")
+        points(Sub_metering_2 ~ datetime, col = "red", type = "l")
+        points(Sub_metering_3 ~ datetime, col = "blue", type = "l")
+        legend("topright",lty = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n", y.intersp = 0.2)
+})
+
+#Plot 4
+with(electricData, plot(Global_reactive_power ~ datetime, xlab = "datetime", ylab = "Global_reactive_power", type = "l"))
+
+#Creating PNG file
+dev.copy(png, file = "plot4.png")
+dev.off()
